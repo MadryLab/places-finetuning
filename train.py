151d17c73d6442eb9cf50c943a347b7e0aec4e92
Logan Engstrom <engstrom@mit.edu>
2022-09-25 10:04:56 -0400

    added

--- train.py
+++ train.py
@@ -6,20 +6,19 @@ import sys
 
 assert sys.version_info.major == 3 and sys.version_info.minor >= 9, 'need python 3.9+'
 
-# TODO remove these
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
 
-# TODO: CHANGE THIS TO THE RIGHT PATH
-PLACES_DATASET = Path('/mnt/cfs/datasets/places365_standard')
-LR = 1.0
-EPOCHS = 2
+PLACES_DATASET = None
+if not PLACES_DATASET:
+    raise NotImplementedError('Need to set PLACES_DATASET to the path of the Places365 dataset')
+
+LR = 0.1
+EPOCHS = 1
 WD = 0
 BS = 256
 NUM_CLASSES = 365
-
-DEBUG_MODE = 0
 
 def make_model():
     model = ch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
@@ -31,7 +30,6 @@ def make_model():
     model = model.cuda().to(memory_format=ch.channels_last)
     return model
 
-# TODO: DELETE THIS
 IMAGENET_MEAN = np.array([0.485, 0.456, 0.406])
 IMAGENET_STD = np.array([0.229, 0.224, 0.225])
 
@@ -40,7 +38,6 @@ def data_postprocess(x, y):
     x = x.to(memory_format=ch.channels_last, non_blocking=True)
     y = y.to(device='cuda', non_blocking=True)
     return x, y
-# TODO: END DELETE THIS
 
 def data_checks(x, y):
     assert x.shape == (BS, 3, 224, 224), x.shape
@@ -48,32 +45,54 @@ def data_checks(x, y):
     assert x.device != ch.device('cpu'), x.device
     assert y.device != ch.device('cpu'), y.device
 
+def get_target_transforms():
+    train_ds = ImageFolder(root=PLACES_DATASET / 'train')
+    val_ds = ImageFolder(root=PLACES_DATASET / 'val')
 
-# TODO: turn this method into a "pass"
-def make_loaders():
-    # pass
+    classes_to_idx = {cls: idx for idx, cls in enumerate(train_ds.classes)}
+
+    def train_target_transform(x):
+        return x
+
+    def val_target_transform(x):
+        curr_label = val_ds.classes[x]
+        idx = train_ds.class_to_idx[curr_label]
+        return idx
+
+    return train_target_transform, val_target_transform
+
+def make_fast_loaders():
     # train_loader = ...
     # val_loader = ...
     # return train_loader, val_loader
+    raise NotImplementedError()
+
+def make_loaders():
     normalize = transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD)
 
+    tr_target_transform, val_target_transform = get_target_transforms()
+
     train_ds = ImageFolder(
-        PLACES_DATASET / 'train', 
-        transforms.Compose([
+        root=PLACES_DATASET / 'train',
+        transform=transforms.Compose([
             transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize
-        ]))
+        ]),
+        target_transform=tr_target_transform)
 
     val_ds = ImageFolder(
-        PLACES_DATASET / 'val',
-        transforms.Compose([
+        root=PLACES_DATASET / 'val',
+        transform=transforms.Compose([
                 transforms.Resize(256),
                 transforms.CenterCrop(224),
                 transforms.ToTensor(),
                 normalize,
-        ]))
+        ]), target_transform=val_target_transform)
+
+    print(val_ds.classes)
+    print(train_ds.classes)
 
     train_loader = DataLoader(train_ds, batch_size=BS, shuffle=True,
                               drop_last=True, num_workers=5)
@@ -89,9 +108,6 @@ def main():
       using the data loading framework assigned to you. While you can modify all
       the code as you see fit, you should only have to modify (a) the `make_loaders`
       function and (b) the training loops. 
-    - You should be able to get TODO\pm 2% accuracy on the validation set with
-      the default hyperparameters and the right
-      dataloading technique.
     '''
     model = make_model()
     model_params = model.fc.parameters()
@@ -114,13 +130,15 @@ def main():
         model.train()
         assert train_loader is not None, 'need to define train loader!'
         for iteration, (x, y) in enumerate(tqdm(train_loader)):
-            x, y = data_postprocess(x, y) # TODO: DELETE THIS
+            x, y = data_postprocess(x, y)
 
-            # you should not have to touch any of this
+            # you should not have to touch anything after this
             with ch.cuda.amp.autocast():
                 data_checks(x, y) # you should not get an error here!
                 out = model(x)
                 loss = losser(out, y)
+                if iteration % 10 == 0:
+                    print('Loss, ', loss.item())
 
             optimizer.zero_grad()
             scaler.scale(loss).backward()
@@ -128,7 +146,7 @@ def main():
             scheduler.step()
             scaler.update()
 
-            if DEBUG_MODE and iteration > 2:
+            if iteration > 5:
                 break
 
         # then test
@@ -136,20 +154,17 @@ def main():
         all_corrects = []
         assert val_loader is not None, 'need to define val loader!'
         for iteration, (x, y) in enumerate(tqdm(val_loader)):
-            x, y = data_postprocess(x, y) # TODO: DELETE THIS
-            # data_checks(x, y) # you should not get an error here!
+            x, y = data_postprocess(x, y)
 
-            # you should not have to touch any of this
+            # you should not have to touch anything after this
             out = model(x)
             corrects = ch.argmax(out, dim=1) == y
             all_corrects.append(corrects)
 
-            if DEBUG_MODE and iteration > 2:
-                break
-
         # all_corrects is bool; cast to 0/1 floats and take mean for accuracy
         acc1 = ch.cat(all_corrects).float().mean()
         print(f'Accuracy @ epoch {epoch}: {acc1}')
+
 
 if __name__ == '__main__':
     main()
